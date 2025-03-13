2887ffe28c2d4fe070b17ce260e18f4a6c41028d
simon-gardier <s.gardier@student.uliege.be>
2025-03-13 18:00:08 +0100

    part 2 statements, part 2 folder

---
+++ part2/driver.cpp
@@ -0,0 +1,138 @@
+/**
+ * @brief Driver for VSOP compiler. Based on course example
+ */
+#include <iostream>
+#include <string>
+#include <map>
+#include "driver.hpp"
+#include "parser.hpp"
+using namespace VSOP;
+
+/**
+ * @brief Map a token type to its string representation for prints.
+ */
+static const std::map<Parser::token_type, std::string> type_to_string = {
+    {Parser::token::TYPE_IDENTIFIER,    "type-identifier"},
+    {Parser::token::OBJECT_IDENTIFIER,  "object-identifier"},
+    {Parser::token::STRING_LITERAL,     "string-literal"},
+    {Parser::token::INTEGER_LITERAL,    "integer-literal"},
+    // Operators
+    {Parser::token::LBRACE,             "lbrace"},
+    {Parser::token::RBRACE,             "rbrace"},
+    {Parser::token::LPAR,               "lpar"},
+    {Parser::token::RPAR,               "rpar"},
+    {Parser::token::COLON,              "colon"},
+    {Parser::token::SEMICOLON,          "semicolon"},
+    {Parser::token::COMMA,              "comma"},
+    {Parser::token::PLUS,               "plus"},
+    {Parser::token::MINUS,              "minus"},
+    {Parser::token::TIMES,              "times"},
+    {Parser::token::DIV,                "div"},
+    {Parser::token::POW,                "pow"},
+    {Parser::token::DOT,                "dot"},
+    {Parser::token::EQUAL,              "equal"},
+    {Parser::token::LOWER,              "lower"},
+    {Parser::token::LOWER_EQUAL,        "lower-equal"},
+    {Parser::token::ASSIGN,             "assign"},
+    {Parser::token::AND,                "and"},
+    //Keywords
+    {Parser::token::BOOL,               "bool"},
+    {Parser::token::CLASS,              "class"},
+    {Parser::token::DO,                 "do"},
+    {Parser::token::ELSE,               "else"},
+    {Parser::token::EXTENDS,            "extends"},
+    {Parser::token::FALSE,              "false"},
+    {Parser::token::IF,                 "if"},
+    {Parser::token::IN,                 "in"},
+    {Parser::token::INT32,              "int32"},
+    {Parser::token::ISNULL,             "isnull"},
+    {Parser::token::LET,                "let"},
+    {Parser::token::NEW,                "new"},
+    {Parser::token::NOT,                "not"},
+    {Parser::token::SELF,               "self"},
+    {Parser::token::STRING,             "string"},
+    {Parser::token::THEN,               "then"},
+    {Parser::token::TRUE,               "true"},
+    {Parser::token::UNIT,               "unit"},
+    {Parser::token::WHILE,              "while"}
+};
+
+/**
+ * @brief Print the information about a token
+ *
+ * @param token the token
+ */
+static void print_token(Parser::symbol_type token)
+{
+    position pos = token.location.begin;
+    Parser::token_type type = (Parser::token_type)token.type_get();
+
+    std::cout   << pos.line << ","
+                << pos.column << ","
+                << type_to_string.at(type);
+
+    if( type == Parser::token::TYPE_IDENTIFIER || 
+        type == Parser::token::OBJECT_IDENTIFIER)
+    {
+        std::string value = token.value.as<std::string>();
+        std::cout << "," << value;
+    }
+    else if (type == Parser::token::STRING_LITERAL)
+    {
+        std::string value = token.value.as<std::string>();
+        std::cout << "," << "\"" << value << "\"";
+    }
+    else if (type == Parser::token::INTEGER_LITERAL)
+    {
+        int value = token.value.as<int>();
+        std::cout << "," << value;
+    }
+    std::cout << std::endl;
+}
+
+/**
+ * @brief Lex the source_file and fill a vector with the tokens found
+ * 
+ * @return int 0 if no lexing error found, 1 else
+ */
+int Driver::lex()
+{
+    scan_begin();
+    int error = 0;
+    while (true)
+    {
+        Parser::symbol_type token = yylex();
+        if ((Parser::token_type)token.type_get() == Parser::token::YYEOF)
+            break;
+        if ((Parser::token_type)token.type_get() != Parser::token::YYerror)
+            tokens.push_back(token);
+        else
+        {
+            error = 1;
+            break;
+        }
+    }
+    scan_end();
+    return error;
+}
+
+/**
+ * @brief Parse the source_file, place holder for part 2.
+ * 
+ * @return int 0 if no parsing error, 1 else 
+ */
+int Driver::parse()
+{
+    std::cout << "Parser will be implemented in part 2" << std::endl;
+    return 0;
+}
+
+/**
+ * @brief Prints all the tokens that have been pushed in the vector tokens
+ * 
+ */
+void Driver::print_tokens()
+{
+    for (auto token : tokens)
+        print_token(token);
+}
---
+++ part2/driver.hpp
@@ -0,0 +1,85 @@
+/**
+ * @brief Driver for VSOP compiler. Based on course example
+ */
+
+ #ifndef _DRIVER_HPP
+ #define _DRIVER_HPP
+ 
+ #include <string>
+ #include <vector>
+ #include <map>
+ 
+ #include "parser.hpp"
+ 
+ // Give prototype of yylex() function, then declare it.
+ #define YY_DECL VSOP::Parser::symbol_type yylex()
+ YY_DECL;
+ 
+ namespace VSOP
+ {
+     class Driver
+     {
+     public:
+         /**
+          * @brief Construct a new Driver.
+          *
+          * @param _source_file The file containing the source code.
+          */
+         Driver(const std::string &_source_file) : source_file(_source_file) {}
+ 
+         /**
+          * @brief Get the source file.
+          *
+          * @return const std::string& The source file.
+          */
+         const std::string &get_source_file() { return source_file; }
+ 
+         /**
+          * @brief Run the lexer on the source file.
+          *
+          * @return int 0 if no lexical error.
+          */
+         int lex();
+ 
+         /**
+          * @brief Run the parser on the source file and compute the result.
+          *
+          * @return int 0 if no syntax or lexical error.
+          */
+         int parse();
+ 
+         /**
+          * @brief Print all the tokens.
+          */
+         void print_tokens();
+ 
+     private:
+         /**
+          * @brief The source file.
+          */
+         std::string source_file;
+ 
+         /**
+          * @brief The parser.
+          */
+         VSOP::Parser *parser;
+ 
+         /**
+          * @brief Store the tokens.
+          */
+         std::vector<Parser::symbol_type> tokens;
+ 
+         /**
+          * @brief Start the lexer.
+          */
+         void scan_begin();
+ 
+         /**
+          * @brief Stop the lexer.
+          */
+         void scan_end();
+     };
+ }
+ 
+ #endif
+ 
